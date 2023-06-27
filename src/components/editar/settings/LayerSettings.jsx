a8794b9bc73c2e80d69a9b8af478623eea2f2796
Gabriel Guzman <gabrielguzman@MacBook-Pro-de-Gabriel.local>
2023-06-27 11:36:15 -0400

    fix layer

--- src/components/editar/settings/LayerSettings.jsx
+++ src/components/editar/settings/LayerSettings.jsx
@@ -7,15 +7,15 @@ export const LayerSettings = () => {
   const { layer, setLayer } = useSetting()
 
   return (
-    <div className='py-5'>
+    <div className='py-5 max-h-96 overflow-y-scroll my-5'>
       <h3 className='text-xl font-medium text-gray-900 mb-6 text-center'>
         Select a layer:
       </h3>
       <div className='flex flex-wrap gap-5 justify-center pb-5'>
         {
           layersTile.map((x, i) => (
-            <div key={i} className={`w-1/4 border cursor-pointer hover:outline hover:outline-yellow-500 p-1 ${layer === x.url && 'outline outline-yellow-500'}`} onClick={() => setLayer(x.url)}>
-              <MapContainer style={{ minHeight: '50px', minWidth: '50px' }} center={[-45.5712, -72.0685]} zoom={10} scrollWheelZoom={false} zoomControl={false} dragging={false}>
+            <div key={i} className={`overflow-y-scroll border cursor-pointer hover:outline hover:outline-yellow-500 p-1 ${layer === x.url && 'outline outline-yellow-500'}`} onClick={() => setLayer(x.url)}>
+              <MapContainer style={{ height: '100px', width: '100px' }} center={[-45.5712, -72.0685]} zoom={10} scrollWheelZoom={false} zoomControl={false} dragging={false}>
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url={x.url}
